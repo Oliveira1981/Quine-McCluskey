8766a9a9ef6203475f97f8cc80ea063d96de7fc4
rodri <rodri@PPGENFSECRETAR2>
2022-10-26 00:07:05 -0300

    Organização de saída.

--- src/main/java/com/mycompany/quine/mccluskey/QuineMcCluskey.java
+++ src/main/java/com/mycompany/quine/mccluskey/QuineMcCluskey.java
@@ -7,53 +7,54 @@ import java.util.ArrayList;
  * @author Rodrigo da Rosa
  */
 public class QuineMcCluskey extends Tools{
-
+    
     public static void main(String[] args) throws Exception  {
         
-        //String litTemplate = "ABCD+!A!BCD+A!B!C!D+!ABCD";
-        //String litTemplate = "A!BCD+!ABC!D+!ABCD+A!B!CD";
-        String litTemplate = "!A*!B*!C*!D + !A*!B*!C*D + !A*B*!C*D + !A*B*C*!D + !A*B*C*D";
-        //String litTemplate = "ab!c+a!bc!d+!ab"; //diferentes tamanhos (definir pelo mintermo maior)
+        //String template = "ABCD+!A!BCD+A!B!C!D+!ABCD";
+        //String template = "A!BCD+!ABC!D+!ABCD+A!B!CD";
+        //String template = "!A*!B*!C*!D + !A*!B*!C*D + !A*B*!C*D + !A*B*C*!D + !A*B*C*D";
+        //String template = "ab!c+a!bc!d+!ab"; //diferentes tamanhos (definir pelo mintermo maior)
         //String litTemplate = "abc+bcd"; //diferentes tamanhos (definir pelo mintermo maior)
+        
         //String binTemplate = "1111+0011+1000+0111";
-        String binTemplate = "1111+0011+1010+0111";
-        //String binTemplate = "111+11+101+01"; //diferentes tamanhos (definir pelo mintermo maior)
-        //String deciTemplate = "15+3+10+7";
-        String deciTemplate = "2+4+6+8+9+10+12+13+15";
+        //String template = "1111+0011+1010+0111";
+        //String template = "111+11+101+01"; //diferentes tamanhos (definir pelo mintermo maior)
+        
+        //String template = "15+3+10+7";
+        String template = "2+4+6+8+9+10+12+13+15";
         
         GUI gui = new GUI();
 
 /////////////////////////////////////////////////////
-        gui.showDialog(deciTemplate);
+        gui.showDialog(template);
 /////////////////////////////////////////////////////
         
         String inputFormat;
         String expression;
-
+        
 /////////////////////////////////////////////////////
         //inputFormat = gui.getInputFormat();
         expression  = gui.getExpression();
+        if(expression == null)
+            System.exit(0);
         inputFormat = inputFormatDetection(expression);
 /////////////////////////////////////////////////////
-
-        if(expression == null) System.exit(0);
         
-        System.out.println("inputFormat:"+inputFormat);
-        System.out.println("expression:"+expression);
-        
-        System.out.println("\n"+inputFormatDetection(expression));
+        System.out.println("Formato de entrada:\n - "+inputFormat);
+        System.out.println("\nExpressão:\n - "+expression);
         
 /////////////////////////////////////////////////////
         SumOfProducts exp = new SumOfProducts(inputFormat, expression);
 /////////////////////////////////////////////////////
 
-        print("\nVARS: "+exp.getNumberOfVars()+"\n");
+        print("\nVariáveis: "+exp.getNumberOfVars()+"\n");
 
 /////////////////////////////////////////////////////
         exp.sortByOnesCount();
 /////////////////////////////////////////////////////
-
-        for(int i=0; i<exp.getProductsList().size(); i++) {
+        
+        print ("\nMintermos por número de 1s:");
+        for(int i=0; i < exp.getProductsList().size(); i++) {
             print("\n");
             print(exp.getProductsList().get(i).getMinTermsList()+"\t");
             print(exp.getProductsList().get(i).getBinaryView()+"\t");
@@ -64,10 +65,10 @@ public class QuineMcCluskey extends Tools{
         exp.mergePrimeImplicants(10);
 /////////////////////////////////////////////////////
 
-        print("\n+++++++++++++++++++++++++++++++\n\n");
-        for(int i=0; i<exp.getProductsList().size(); i++) {
+        print("\n\nImplicantes primos mesclados:\n");
+        for(int i=0; i < exp.getProductsList().size(); i++) {
             int q = 0;
-            for(; q<exp.getProductsList().get(i).getMinTermsList().size(); q++) {
+            for(; q < exp.getProductsList().get(i).getMinTermsList().size(); q++) {
                 print("-"+exp.getProductsList().get(i).getMinTermsList().get(q));
             }
             if(q < 3)
@@ -80,33 +81,34 @@ public class QuineMcCluskey extends Tools{
 /////////////////////////////////////////////////////
         exp.fillMinTermsList();
 /////////////////////////////////////////////////////
-
-        for (int i=0; i<exp.getMinTermsList().size(); i++) {
+        
+        print ("\nMintermos e seus produtos (Tabela de Cobertura):");
+        for (int i=0; i < exp.getMinTermsList().size(); i++) {
             print("\n"+exp.getMinTermsList().get(i).getDecimalView()+" -");
-            for (int p=0; p<exp.getMinTermsList().get(i).getProductsList().size(); p++) {
+            for (int p=0; p < exp.getMinTermsList().get(i).getProductsList().size(); p++) {
                 print("\t\t"+exp.getMinTermsList().get(i).getProductsList().get(p).getLiteralView());//.get(0));
+                if (exp.getMinTermsList().get(i).getProductsList().get(p).getLiteralView().length() < 8)
+                    print("\t");
             }
         }
         
 /////////////////////////////////////////////////////
         exp.essentialProductsToFinalList();
 /////////////////////////////////////////////////////
-
-        print("\n\nEssential Products List:\n");
-        for (int i=0; i<exp.getFinalProductsList().size(); i++) {
+        
+        print("\n\nProdutos Essenciais:\n");
+        for (int i=0; i < exp.getFinalProductsList().size(); i++) {
             print(exp.getFinalProductsList().get(i).getLiteralView()+"\t");
         }
         
 /////////////////////////////////////////////////////
         ArrayList<Integer> indexes = exp.getCandidateProductsIndexes();
 /////////////////////////////////////////////////////
-        
-        printarr(indexes);
-        
-        for (int i=0; i<exp.getMinTermsList().size(); i++) {
+        print("\n");
+        for (int i=0; i < exp.getMinTermsList().size(); i++) {
             print("\nMinTerm "+exp.getMinTermsList().get(i).getDecimalView()+" ");
             if (exp.getMinTermsList().get(i).isIsCovered())
-                print("is covered.");
+                print("está coberto.");
             else
                 print(" - ");
         }
@@ -123,8 +125,8 @@ public class QuineMcCluskey extends Tools{
         exp.completeFinalList();
 /////////////////////////////////////////////////////
 
-        print("\nFinal Products List:\n");
-        for (int i=0; i<exp.getFinalProductsList().size(); i++) {
+        print("\n\nProdutos Finais:\n");
+        for (int i=0; i < exp.getFinalProductsList().size(); i++) {
             print(exp.getFinalProductsList().get(i).getLiteralView()+"\t");
         }
         
@@ -132,7 +134,8 @@ public class QuineMcCluskey extends Tools{
         exp.setOptimizedExpression();
 /////////////////////////////////////////////////////
 
-        print("\n\n"+exp.getOptimizedExpression()+"\n");
+        print("\n\nExpressão otimizada:\n");
+        print(exp.getOptimizedExpression()+"\n\n");
         
     }
 }
